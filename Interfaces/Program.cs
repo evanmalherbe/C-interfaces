d56a98864466ca93d461faade1ce7e8b9cf59e81
evanmalherbe <evanmalherbe@gmail.com>
2022-12-16 08:18:07 +0200

    First commit

---
+++ Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadVideoActivity loadVideoActivity = new LoadVideoActivity();
+            CallWebserviceActivity callWebserviceActivity = new CallWebserviceActivity();
+
+            WorkFlow workflow = new WorkFlow();
+            workflow.AddWorkFlow(loadVideoActivity);
+            workflow.AddWorkFlow(callWebserviceActivity);
+
+            List<IActivity> list = workflow.GetWorkFlow();
+
+            WorkflowEngine workflowEngine = new WorkflowEngine();
+            workflowEngine.Run(list);
+        }
+    }
+}
---
+++ Interfaces/WorkFlow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class WorkFlow
+    {
+        private readonly List<IActivity> _workflow;
+
+        public WorkFlow()
+        {
+            _workflow = new List<IActivity>();
+        }
+
+        public void AddWorkFlow(IActivity activity)
+        {
+            _workflow.Add(activity);
+        }
+
+        public List<IActivity> GetWorkFlow()
+        {
+            return _workflow;
+        }
+    }
+}
